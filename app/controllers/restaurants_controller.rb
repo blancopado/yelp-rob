8405b089de8f466669eec9bf6ab1d88c7b6f44f8
blancopado <pablo.dominguezblanco@gmail.com>
2015-11-27 12:55:18 +0100

    adding images to restaurants

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -44,6 +44,7 @@ class RestaurantsController < ApplicationController
   private
 
   def restaurant_params
-    params.require(:restaurant).permit(:name)
+    params.require(:restaurant).permit(:name, :image)
   end
+
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,9 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "300x300>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   def average_rating
   	return 'N/A' if reviews.none?
   	reviews.inject(0) {|memo, review| memo + review.rating} / reviews.length
@@ -12,5 +15,6 @@ class Restaurant < ActiveRecord::Base
     attributes[:user] ||= user
     reviews.build(attributes)
   end
+
 
 end
