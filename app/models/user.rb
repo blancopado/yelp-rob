ad91b0c2273fa2b632eba4e222dc582030acd3c3
jamierose <jrose111284@gmail.com>
2015-11-27 10:26:45 +0000

    Getting started with Paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   def has_reviewed?(restaurant)
     reviewed_restaurants.include? restaurant
